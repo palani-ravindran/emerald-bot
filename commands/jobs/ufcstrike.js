c52cd72f71d8d8c3fdef583ac16204a91ac0b5f8
jacob-tucker <jacobtucker818@gmail.com>
2022-06-27 14:33:18 -0400

    try

--- commands/jobs/ufcstrike.js
+++ commands/jobs/ufcstrike.js
@@ -52,9 +52,9 @@ async function toRemove(discordIds) {
                   found = true
                   break
                 }
-                found = true
-                break
               }
+              found = true
+              break
             }
           }
           if !found {
